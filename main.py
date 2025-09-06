f322c0d11e682f08006bbbf03d65116130824c4e
celsz <none@none.ru>
2025-09-06 16:26:23 +0300

    Homework 1 tasks 1.1, 1.2, 1.3

--- main.py
+++ main.py
@@ -14,6 +14,30 @@ def task1_2():
     temperature = input("Enter a temperature: ")
 
     try:
+        temperature = int(temperature)
+        if temperature <= 0:
+            print("VERY COLD!")
+        elif 0 < temperature <= 20:
+            print("COLD!")
+        else:
+            print("WARM")
 
 
-task1_1()
+    except ValueError:
+        print("Invalid input")
+
+def task1_3():
+    time = input("Enter a time: ")
+    try:
+        time = int(time)
+        if 6 <= time <= 11:
+            print("Утро")
+        elif 12 <= time <= 17:
+            print("День")
+        elif 18 <= time <= 22:
+            print("Вечер")
+        elif time == 23 or time <= 5:
+            print("Ночь")
+    except ValueError:
+        print("Invalid input")
+
